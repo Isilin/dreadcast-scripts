0ca05aae216622c7180518e8e0346e1610576349
IsilinBN <casa2pir@hotmail.fr>
2024-09-03 21:17:01 +0200

    [Add] example

---
+++ examples/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  // Loading a spreadhseet en displaying the result.
+  DC.DB.loadSpreadsheet(
+    '1Ygt9q6WEU8cR_86GptLpHZ6qLHATfX42R0qcPKaqvqo',
+    'BDD',
+    'A:C',
+    'AIzaSyCSnNrK0PQMz20JVuUmuO9rl9iSWRHrPm4',
+    (response) => console.log(response),
+  );
+
+  // Modifying the top menu
+  DC.TopMenu.add(DC.UI.Separator(), 3);
+  DC.TopMenu.add(
+    DC.UI.DropMenu('Test', [
+      DC.UI.SubMenu(
+        'Ping!',
+        () =>
+          DC.UI.PopUp(
+            'test45',
+            `Test`,
+            `<h1>Test de fou de la mort qui tue !</h1>`,
+          ),
+        true,
+      ),
+    ]),
+    4,
+  );
+  DC.UI.addSubMenuTo(
+    'Paramètres ▾',
+    DC.UI.SubMenu('Ping!', () => console.log('Pong!'), true),
+    4,
+  );
+
+  // Adding to sidemenu on the right
+  DC.UI.SideMenu('test', 'Test', '<p>Coucou</p>');
+  DC.UI.SideMenu('test2', 'Test 2', '<p>Coucou encore</p>');
+
+  // Using the BBCode DC-like.
+  console.log(
+    DC.Chat.t('coucou', { bold: true, italic: true, color: 'ababab' }),
+  );
+
+  // Intercepting chat events
+  nav.getChat().onSend((next, abort) => {
+    console.log('BEFORE');
+    return next();
+  });
+  nav.getChat().onAfterSend(() => {
+    console.log('AFTER');
+  });
+});
