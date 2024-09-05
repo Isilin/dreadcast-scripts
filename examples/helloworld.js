456145655471514133a41ce0cbe76a0efee5a9ff
IsilinBN <casa2pir@hotmail.fr>
2024-09-06 01:29:01 +0200

    [Fix] click event settings hello world test. (5)

--- examples/helloworld.js
+++ examples/helloworld.js
@@ -1,7 +1,7 @@
 $(() => {
   console.log('Hello World!');
 
-  $('#hello_world_setting').bind('click', () =>
+  $(document).on('click', '#hello_world_setting', () =>
     DC.UI.PopUp('hello_world_popup', 'Test', $('<p>YES</p>')),
   );
 });
