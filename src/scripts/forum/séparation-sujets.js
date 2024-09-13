771d166fa4204b87099aa297c693d22a0b4745c1
IsilinBN <casa2pir@hotmail.fr>
2024-09-13 05:12:31 +0200

    [Fix] remove functionnality to hide topics as it can be forbidden.

--- src/scripts/forum/séparation-sujets.js
+++ src/scripts/forum/séparation-sujets.js
@@ -7,7 +7,7 @@
 // @match       https://www.dreadcast.net/FAQ#
 // @match       https://www.dreadcast.net/Forum/*
 // @match       https://www.dreadcast.net/FAQ/*
-// @version     2.2.1
+// @version     2.2.2
 // @author      Aversiste, MockingJay, Odul, Pelagia
 // @description Separe le RP du HRP dans la section 'Derniers Sujets'.
 // @license     http://creativecommons.org/licenses/by-nc-nd/4.0/
@@ -105,76 +105,46 @@
     });
   };
 
-  function addClickEventHide(node) {
-    node.bind('click', () => {
-      var currentTopicId = getId(window.location.href);
-      if (data.blacklist.includes(currentTopicId)) {
-        data.blacklist.splice(data.blacklist.indexOf(currentTopicId, 1));
-        $('#hideTopic').text('Masquer ce sujet');
-      } else {
-        data.blacklist.push(currentTopicId);
-        $('#hideTopic').text('Afficher ce sujet');
-      }
-      DC.LocalMemory.set(DATA_TAG, data);
-    });
-  }
-
   //****************
   //***DEBUT MAIN***
   //****************
   $(document).ready(() => {
-    try {
-      initPersistence();
+    initPersistence();
 
-      var origList = $('#list_derniers_sujets'); // Récupération du div des derniers sujets
-      blacklist(origList); //Masquage de tous les topics qui doivent l'être, avant clonage et tri des catégories
+    var origList = $('#list_derniers_sujets'); // Récupération du div des derniers sujets
+    blacklist(origList); //Masquage de tous les topics qui doivent l'être, avant clonage et tri des catégories
 
-      const sections = [
-        {
-          id: 'hrp',
-          name: 'Derniers Sujets HRP',
-          categories: [3, 4, 7, 8, 9, 10],
-        },
-        { id: 'rp', name: 'Derniers Sujets RP', categories: [12, 13, 14, 15] },
-        { id: 'ecoreb', name: 'Matrice Rebelle', categories: [20] },
-        { id: 'polreb', name: 'Politique Rebelle', categories: [19] },
-        { id: 'ecoimp', name: 'Matrice Impériale', categories: [18] },
-        { id: 'polimp', name: 'Politique Impériale', categories: [17] },
-        { id: 'annonces', name: 'Annonces Officielles', categories: [2, 5] },
-      ];
+    const sections = [
+      {
+        id: 'hrp',
+        name: 'Derniers Sujets HRP',
+        categories: [3, 4, 7, 8, 9, 10],
+      },
+      { id: 'rp', name: 'Derniers Sujets RP', categories: [12, 13, 14, 15] },
+      { id: 'ecoreb', name: 'Matrice Rebelle', categories: [20] },
+      { id: 'polreb', name: 'Politique Rebelle', categories: [19] },
+      { id: 'ecoimp', name: 'Matrice Impériale', categories: [18] },
+      { id: 'polimp', name: 'Politique Impériale', categories: [17] },
+      { id: 'annonces', name: 'Annonces Officielles', categories: [2, 5] },
+    ];
 
-      //Clonage et tri des nouvelles catégories. Dans l'ordre inversé, car utilisation de prepend.
-      sections.forEach(($section) => {
-        createSectionNode(
-          origList,
-          $section.id,
-          $section.name,
-          $section.categories,
-        );
-      });
+    //Clonage et tri des nouvelles catégories. Dans l'ordre inversé, car utilisation de prepend.
+    sections.forEach(($section) => {
+      createSectionNode(
+        origList,
+        $section.id,
+        $section.name,
+        $section.categories,
+      );
+    });
 
-      origList.remove(); //Enlever la liste originale une fois le tri effectué.
+    origList.remove(); //Enlever la liste originale une fois le tri effectué.
 
-      //Ne pas afficher une catégorie si elle est vide
-      $('#menu_droite > div > ul').each(function () {
-        if ($(this).text().trim() === '') {
-          $(this).parent().css('display', 'none');
-        }
-      });
-
-      //Ajout du lien pour activer la fonction 'cacher'
-      if ($('#header_sujet').length) {
-        $('.list_tags').css('display', 'flex').css('gap', '0.5rem');
-        const hideTopic = $(
-          '<div id="hideTopic" class="link" style="text-align: right;">Masquer ce sujet</div>',
-        ).appendTo($('.list_tags'));
-        if (data.blacklist.includes(getId(window.location.href))) {
-          $('#hideTopic').text('Afficher ce sujet');
-        }
-        addClickEventHide(hideTopic);
+    //Ne pas afficher une catégorie si elle est vide
+    $('#menu_droite > div > ul').each(function () {
+      if ($(this).text().trim() === '') {
+        $(this).parent().css('display', 'none');
       }
-    } catch (err) {
-      console.error(error);
-    }
+    });
   });
 })();
